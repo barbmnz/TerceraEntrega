5ee800f1eba4468125f2e16a4ea4917fd827e55d
Barbitch <154478533+barbmnz@users.noreply.github.com>
2024-04-25 19:13:43 -0400

    productoslista.js

---
+++ productoslista.js
@@ -0,0 +1,49 @@
+const productosLista = [{
+    id: 1,
+    foto: "https://i.etsystatic.com/23563576/r/il/ad31f5/2989790283/il_1588xN.2989790283_eaqn.jpg",
+    nombre: "Chaqueta de mezclilla",
+    precio: 400
+},
+{
+    id: 2,
+    foto: "https://images-na.ssl-images-amazon.com/images/I/61K6%2ByAl7tL._AC_UX679_.jpg",
+    nombre: "Chaqueta de cuero",
+    precio: 600
+},
+{
+    id: 3,
+    foto: "https://i.pinimg.com/originals/6b/3d/4c/6b3d4cd521bbae56fe9578fd84297ba2.jpg",
+    nombre: "Chaqueta Invierno",
+    precio: 1000
+},
+{
+    id: 4,
+    foto: "https://asset1.cxnmarksandspencer.com/is/image/mands/SD_01_T59_1223C_BN_X_EC_90?wid=1024&qlt=80",
+    nombre: "Abrigo invierno",
+    precio: 800
+},
+{
+    id: 5,
+    foto: "https://www.backcountry.com/images/items/1200/COL/COL00M8/DOLSEAICE.jpg",
+    nombre: "Cortaviento",
+    precio: 900
+},
+{
+    id: 6,
+    foto: "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2015/10/22/14455089473793.jpg",
+    nombre: "Chaqueta Marinero",
+    precio: 1300
+},
+{
+    id: 7,
+    foto: "https://i.pinimg.com/originals/b0/e0/4a/b0e04abec16b064041bf154530c63608.jpg",
+    nombre: "Abrigo Verde",
+    precio: 1300
+},
+{
+    id: 8,
+    foto: "https://api.ellecanada.com/app/uploads/2015/10/c4a2e082-ec90-473e-9cc7-3f37660b524c-64852-1.jpeg",
+    nombre: "Sweater Largo",
+    precio: 600
+}
+];
